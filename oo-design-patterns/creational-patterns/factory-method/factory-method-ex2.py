20d8f01aeebacef25cda2ca523d5e6fcafe7323e
JoshuaHenriques <joshuahenriques95@gmail.com>
2024-06-01 17:57:17 -0400

    remove unnecessary comments

--- oo-design-patterns/creational-patterns/factory-method/factory-method-ex2.py
+++ oo-design-patterns/creational-patterns/factory-method/factory-method-ex2.py
@@ -9,12 +9,6 @@ class LocalizerFactory(ABC):
     
     @abstractmethod
     def create_localizer(self):
-        """
-        Factory Method: Create a Localizer instance.
-        
-        Returns:
-            Localizer: An instance of a concrete Localizer.
-        """
         pass
 
 
@@ -26,18 +20,6 @@ class FrenchLocalizerFactory(LocalizerFactory):
     """
     
     def create_localizer(self):
-        """
-        Factory Method implementation for creating FrenchLocalizer.
-        
-        Returns:
-            FrenchLocalizer: An instance of FrenchLocalizer.
-        
-        Example:
-            >>> factory = FrenchLocalizerFactory()
-            >>> localizer = factory.create_localizer()
-            >>> localizer.localize("car")
-            'voiture'
-        """
         return FrenchLocalizer()
 
 
@@ -49,18 +31,6 @@ class SpanishLocalizerFactory(LocalizerFactory):
     """
     
     def create_localizer(self):
-        """
-        Factory Method implementation for creating SpanishLocalizer.
-        
-        Returns:
-            SpanishLocalizer: An instance of SpanishLocalizer.
-        
-        Example:
-            >>> factory = SpanishLocalizerFactory()
-            >>> localizer = factory.create_localizer()
-            >>> localizer.localize("bike")
-            'bicicleta'
-        """
         return SpanishLocalizer()
 
 
@@ -72,18 +42,6 @@ class EnglishLocalizerFactory(LocalizerFactory):
     """
     
     def create_localizer(self):
-        """
-        Factory Method implementation for creating EnglishLocalizer.
-        
-        Returns:
-            EnglishLocalizer: An instance of EnglishLocalizer.
-        
-        Example:
-            >>> factory = EnglishLocalizerFactory()
-            >>> localizer = factory.create_localizer()
-            >>> localizer.localize("cycle")
-            'cycle'
-        """
         return EnglishLocalizer()
 
 class Localizer(ABC):
@@ -95,15 +53,6 @@ class Localizer(ABC):
     
     @abstractmethod
     def localize(self, msg):
-        """
-        Translate the given message.
-        
-        Args:
-            msg (str): The message to be translated.
-            
-        Returns:
-            str: The translated message.
-        """
         pass
 
 class FrenchLocalizer(Localizer):
@@ -121,20 +70,6 @@ class FrenchLocalizer(Localizer):
         }
 
     def localize(self, msg):
-        """
-        Translate the message to French.
-        
-        Args:
-            msg (str): The message to be translated.
-            
-        Returns:
-            str: The translated message.
-        
-        Example:
-            >>> localizer = FrenchLocalizer()
-            >>> localizer.localize("car")
-            'voiture'
-        """
         return self.translations.get(msg, msg)
 
 
@@ -153,20 +88,6 @@ class SpanishLocalizer(Localizer):
         }
 
     def localize(self, msg):
-        """
-        Translate the message to Spanish.
-        
-        Args:
-            msg (str): The message to be translated.
-            
-        Returns:
-            str: The translated message.
-        
-        Example:
-            >>> localizer = SpanishLocalizer()
-            >>> localizer.localize("bike")
-            'bicicleta'
-        """
         return self.translations.get(msg, msg)
 
 
@@ -178,20 +99,6 @@ class EnglishLocalizer(Localizer):
     """
     
     def localize(self, msg):
-        """
-        Return the message as is (no translation).
-        
-        Args:
-            msg (str): The message to be translated.
-            
-        Returns:
-            str: The same message.
-        
-        Example:
-            >>> localizer = EnglishLocalizer()
-            >>> localizer.localize("cycle")
-            'cycle'
-        """
         return msg
     
 if __name__ == "__main__":
@@ -224,25 +131,6 @@ localizers without separate concrete factory classes.
 # def create_localizer(language="English"):
 #     """
 #     Factory Method: Create a localizer for the specified language.
-
-#     Args:
-#         language (str): The language for which to create a localizer.
-
-#     Returns:
-#         Localizer: An instance of the localizer for the specified language.
-
-#     Example:
-#         >>> french_localizer = create_localizer("French")
-#         >>> french_localizer.localize("car")
-#         'voiture'
-        
-#         >>> english_localizer = create_localizer("English")
-#         >>> english_localizer.localize("bike")
-#         'bike'
-        
-#         >>> spanish_localizer = create_localizer("Spanish")
-#         >>> spanish_localizer.localize("cycle")
-#         'ciclo'
 #     """
 #     localizers = {
 #         "French": FrenchLocalizer,
